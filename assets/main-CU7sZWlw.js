6523318422bbd9b0e4b5e9e58c5a6dc92482a0f2
StepanxXx <stepanxxx@gmail.com>
2026-07-01 10:31:44 +0000

    Deploying to gh-pages from @ StepanxXx/dragon-quest@4ee72496816ae4efd12575dd75a0b770a885053f 🚀

---
+++ assets/main-CU7sZWlw.js
@@ -0,0 +1,2 @@
+import{i as e}from"./rating-modal-DY_er_JA.js";const i=document.getElementById("loader");function o(){i&&i.classList.add("is-active")}function t(){i&&i.classList.remove("is-active")}o();t();e();
+//# sourceMappingURL=main-CU7sZWlw.js.map
--- assets/main-CpOvIso0.js
+++
@@ -1,2 +0,0 @@
-import{i}from"./rating-modal-BjyVZe3B.js";i();
-//# sourceMappingURL=main-CpOvIso0.js.map

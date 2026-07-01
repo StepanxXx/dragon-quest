0eb4aeebb33b544442f64d75a48615f8c975806e
d3-bogdan-pysarenko <53533112+d3-bogdan-pysarenko@users.noreply.github.com>
2026-07-01 09:34:59 +0000

    Deploying to gh-pages from @ StepanxXx/dragon-quest@6644bfe9f6f4f0b2d7c2d5135bfe36c4fea7b7d2 🚀

---
+++ assets/main-CpOvIso0.js
@@ -0,0 +1,2 @@
+import{i}from"./rating-modal-BjyVZe3B.js";i();
+//# sourceMappingURL=main-CpOvIso0.js.map
--- assets/main-DC73I04r.js
+++
@@ -1,2 +0,0 @@
-import{i}from"./rating-modal-CgrpIiFT.js";i();
-//# sourceMappingURL=main-DC73I04r.js.map
